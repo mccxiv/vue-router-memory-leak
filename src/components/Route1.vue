3ebe9bd36e17a4502a088fa489ba0641bd0d221b
Andrea Stella <adr.stella@gmail.com>
2022-09-28 13:56:39 +0200

    Try with element-plus

--- src/components/Route1.vue
+++ src/components/Route1.vue
@@ -1,4 +1,5 @@
 <template>
-  <p>Route 1 has lots of items</p>
+  <p>Route 1 has el-autocomplete and lots of DOM items</p>
+  <el-autocomplete placeholder="Placeholder"></el-autocomplete>
   <div v-for="index in 10000"> {{ index }} </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import Route1 from './components/Route1.vue'
 import Route2 from './components/Route2.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -19,5 +21,6 @@ const router = createRouter({
 })
 
 const app = createApp(App)
+app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
